12707fb9d79230b430674aac642c2aed87a85b3d
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 06:01:54 +0100

    Task annotation and async

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Floor Annotated Function Task """
+
+
+def floor(n: float) -> int:
+    """Return the floor of a number, rounded down to the nearest integer.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The floor of the number.
+    """
+    return int(n)
