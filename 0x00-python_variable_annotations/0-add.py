d4ce5a93b9629540e1d371e1503e90d622183cac
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 05:47:15 +0100

    Task annotation and async

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
+
 def add(a: float, b: float) -> float:
+    """Function to add two float values"""
     return a + b
--- 0x00-python_variable_annotations/0-main.py
+++ 0x00-python_variable_annotations/0-main.py
@@ -3,3 +3,4 @@ add = __import__('0-add').add
 
 print(add(1.11, 2.22) == 1.11 + 2.22)
 print(add.__annotations__)
+print(__import__("0-add").add.__doc__)
