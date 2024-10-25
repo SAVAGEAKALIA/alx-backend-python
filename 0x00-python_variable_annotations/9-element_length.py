a8a1411be8de92e55b2ac3c5f4424c836465de20
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 07:22:36 +0100

    Task annotation and async

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,6 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a list of tuples,
+    where each tuple contains a sequence and its length."""
     return [(i, len(i)) for i in lst]
