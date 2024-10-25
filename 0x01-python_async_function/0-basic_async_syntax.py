b9db631756eb7bfdf8a6c8f5c21703640ab45eb2
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 05:43:45 +0100

    Task annotation and async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ Basic async function code"""
     delay = random.uniform(0, max_delay)
     # print(f"Waiting random time...{delay:0.2f}")
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """Measure the time taken for n coroutines to complete,
     with each coroutine having a random delay up to max_delay seconds.
     """
