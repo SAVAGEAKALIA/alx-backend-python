29385aa48f0ed738a3b45cf2a5b249dca87cd740
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 06:33:23 +0100

    Task annotation and async

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Function Task that take a list of float to return sum"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Calculate the sum of all elements in the input list using a generator expression.
 
     Args:
