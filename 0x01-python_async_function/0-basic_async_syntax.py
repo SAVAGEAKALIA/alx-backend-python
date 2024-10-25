6ca936231c2933252d205734b6be02033dd67b1d
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 08:50:31 +0100

    Task annotation and async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""Task 0: The basics of async"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ Basic async function code"""
+    """ Basic async function code
+        :param: max_delay:
+    """
     delay = random.uniform(0, max_delay)
     # print(f"Waiting random time...{delay:0.2f}")
     await asyncio.sleep(delay)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronously function to spawn wait_random n times"""
     async_list = []
     for i in range(n):
