750ddb41e3b116ba77351b3cf5f3ffcb9a2b2dcd
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 05:58:14 +0100

    Task annotation and async

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Task tos how annotation in strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate two strings and return the result
+
+    Args:
+        str1 (string): First string
+        str2 (string): Second string
+    """
+    return str1 + str2
