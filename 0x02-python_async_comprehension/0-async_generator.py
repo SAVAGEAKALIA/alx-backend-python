a0cdda573ac3e2f71083c221155a7536dfc3c192
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-30 07:51:29 +0100

    async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Task 0:  Async Generator"""
 
-from typing import AsyncGenerator
+from typing import Generator
 import random
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Function that loops 10 times to yield random numbers"""
     for i in range(10):
         await asyncio.sleep(1)
